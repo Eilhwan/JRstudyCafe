c27a74b29f6db3f378e3209ff4f0381a8cd316fc
JeonEilhwan <https://github.com/Eilhwan/Programmers>
2020-04-08 16:22:06 +0900

    firstcommit

---
+++ 2nd_project/src/main/java/com/tj/pro/Application.java
@@ -0,0 +1,14 @@
+package com.tj.pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
